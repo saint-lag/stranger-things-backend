add06be2b4d076bc62c262ccde04006649e35247
saint-lag <maiasena.gabriel@gmail.com>
2022-09-22 14:07:54 -0300

    feat: updates Dockerfile, index.js and .env

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = JSON.parse(UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
